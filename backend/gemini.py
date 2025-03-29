09bc8c2b54ac120fba422e0278ef2a6ddb6fb43f
Corey Loftus <corey.loftus@syncsmart.io>
2025-03-29 14:25:01 -0400

    simple gemini prompt post route

--- backend/gemini.py
+++ backend/gemini.py
@@ -1,6 +1,19 @@
 import os
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
-# Load environment variables from .env file
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+
+def gemini_text_call(
+    prompt="Please give me a cursory overview of using rhetoric in english writing.",
+):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=[prompt],
+    )
+    print(response.text)
+    return response
