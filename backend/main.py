d1889e4943c1aa46262e4a1d53617004d95a251d
Corey Loftus <coreyloftus@gmail.com>
2025-04-13 14:59:58 -0400

    write gemini audio call

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,19 @@ from typing import Union, Literal
 from fastapi import FastAPI
 from pydantic import BaseModel
 from gemini import gemini_text_call
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["http://localhost:3000", "http://192.168.86.77:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET", "PUT"],
+    allow_headers=["*"],
+)
 
 class Item(BaseModel):
     name: str
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Answering Machine Backend
+
+## Getting Started
+
+Clone the repo into a local directory
+
+`git repo clone https://github.com/coreyloftus/answering-machine-backend.git`
+
+create a virtual environment
+
+`python3 -m venv venv`
+
+install dependencies
+
+`pip install -q -U google-genai fastapi dotenv`
